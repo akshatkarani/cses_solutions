0badd58e51817ef41925835c4dda5c4bcf197e8b
akshat <akshat.karani@needl.ai>
2023-06-03 17:37:41 +0530

    Added basic template

---
+++ template.py
@@ -0,0 +1,15 @@
+# Problem name
+
+
+def solution(*args):
+    pass
+
+
+def main():
+    input_list = input().split()
+    output = solution(*input_list)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
